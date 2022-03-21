13a2c9ad3dedb63df66ccaeb7c735a7c0929eab0
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-03-21 22:51:45 +0600

    added nav bar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
     return (
-        <div>
-            <h1>Hello world!</h1>
+        <div className="App">
+            <Header />
         </div>
     );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <nav>
+            <div className="header">
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div className="links">
+                    <a href="/shop">Shop</a>
+                    <a href="/orders">Order Review</a>
+                    <a href="/inventory">Manage Inventory</a>
+                    <a href="/about">About</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
